13ccf3012ee1a9275fe3b07c74f441da89c6e4a0
Gaurav Ghosh <gauravghosh.gsquared@gmail.com>
2016-05-29 22:26:26 +0530

    Change FB_VERIFY_TOKEN to match WeatherBot

--- const.js
+++ const.js
@@ -11,7 +11,7 @@ const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 if (!FB_VERIFY_TOKEN) {
-  FB_VERIFY_TOKEN = "just_do_it";
+  FB_VERIFY_TOKEN = "a613af2ba9aa5fd5ef5299cfdbe1266e65b985a8";
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.post('/webhook', (req, res) => {
       // This will run all actions until our bot has nothing left to do
       wit.runActions(
         sessionId, // the user's current session
-        msg, // the user's message 
+        msg, // the user's message
         sessions[sessionId].context, // the user's current session state
         (error, context) => {
           if (error) {
